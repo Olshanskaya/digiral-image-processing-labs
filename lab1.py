40966ed059b1e9eab6a6da6022aaefc634cbc858
ViktoriaRubenovna <ViktoriaRubenovna@gmail.com>
2020-10-02 12:10:16 +0300

    lab1_passed

--- lab1.py
+++ lab1.py
@@ -16,20 +16,18 @@ def round_fun(img2, a, b):
 
 
 if __name__ == '__main__':
-    img = cv2.imread("Lenna.png")
+    img = cv2.imread("Bildschirmfoto.2016.04.06.um_.00.30.50.jpg")
     # cv2.imshow("orig", img)
 
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     assert isinstance(gray_img, object)
-    histg = np.zeros((255, 1))
+    histg = np.zeros((256, 1))
 
     for y in range(gray_img.shape[0]):
         for x in range(gray_img.shape[1]):
             i = gray_img[x, y]
             histg[i] = int(histg[i] + 1)
-
     histg_cv = cv2.calcHist([gray_img], [0], None, [256], [0, 256])
-    # cv2.imshow("gray_img", gray_img)
 
     # linear transforms
     linear_image = np.zeros(gray_img.shape, gray_img.dtype)
@@ -56,6 +54,8 @@ if __name__ == '__main__':
             linear_image[y, x] = np.clip(round_fun(gray_img, alpha, beta), 0, 255)
     histg_linear = cv2.calcHist([linear_image], [0], None, [256], [0, 256])
 
+    cv2.imwrite('linear_image.jpg', linear_image)
+
     # equalization
     equ_image_cv = cv2.equalizeHist(gray_img)
 
@@ -71,18 +71,35 @@ if __name__ == '__main__':
             equ_image[y, x] = np.clip(trans_table[gray_img[y, x]], 0, 255)
 
     histg_equalize = cv2.calcHist([equ_image], [0], None, [256], [0, 256])
+    cv2.imwrite('equalize.jpg', equ_image)
 
     # visualization
     ideal = cv2.imread("Lenna_orig.png", 0)
     vis = np.concatenate((gray_img, equ_image), axis=1)
     vis = np.concatenate((vis, linear_image), axis=1)
     cv2.imshow("orig equalize linear", vis)
+    cv2.imwrite('equalize.jpg', vis)
+    fig2 = plt.figure()
+    ax = fig2.add_subplot()
+    steps = [x for x in range(0, 256)]
+    print(steps)
+    # print(histg_linear.list())
+    ax.bar(steps, histg_linear.transpose().tolist()[0], 1, color="black")
+    ax.bar(steps, histg.transpose().tolist()[0], 1, color ="blue")
+    ax.bar(steps, histg_equalize.transpose().tolist()[0], 1, color ="green")
+    # plt.hist(gray_img, edgecolor='blue', linewidth=1.2)
+    # plt.hist(equ_image, edgecolor='yellow', linewidth=2)
+    # plt.hist(linear_image, edgecolor='black', linewidth=2)
+
+    plt.show()
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
     '''
     plt.plot(histg, "b")
     plt.plot(histg_equalize, "r")
     plt.plot(histg_linear, "g")
-    '''
+    
     fig, ax = plt.subplots()
     x = np.arange(1, len(histg.transpose().tolist()[0]) + 1)
     ax.bar(x, histg.transpose().tolist()[0])
@@ -98,15 +115,12 @@ if __name__ == '__main__':
 
     plt.show()
 
-    '''
+    
     plt.hist(gray_img)
     plt.hist(equ_image)
     plt.hist(gray_img)
     plt.show()
     '''
-
-    cv2.waitKey()
-    cv2.destroyAllWindows()
 
     '''
     vals = img.mean(axis=2).flatten()
